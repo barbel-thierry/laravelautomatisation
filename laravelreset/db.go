28da6500e07f93f81a4a171e3f3c67db92c69be4
kaynee <barbel.thierry@gmail.com>
2020-04-12 23:20:21 +0200

    unknown mysql container for reset action

--- laravelreset/db.go
+++ laravelreset/db.go
@@ -21,8 +21,10 @@ func GetDbName() string {
 // CreateDb logs into docker container to try and create mysql DB.
 func CreateDb(db string, container string) {
 	if container == "" {
-		container = "docker_db_1"
+		fmt.Println("Error loading .env file")
+		os.Exit(1)
 	}
+	
 	createDb := "CREATE DATABASE IF NOT EXISTS " + db + ";"
 	create := exec.Command("docker", "exec", container, "mysql", "-u", os.Getenv("DB_USERNAME"), "-p" + os.Getenv("DB_PASSWORD"), "-e", createDb)
 	create.Run()
