9a49c78e03c0b80b67df06eb3010bc6429f88d07
barbel-thierry <barbel.thierry@gmail.com>
2020-10-14 08:48:56 +0200

    cmd

--- laravelnew/create.go
+++ laravelnew/create.go
@@ -4,10 +4,11 @@ import "os/exec"
 
 // create launches Laravel 'new' command.
 func create(project string, version string) {
+	cmd := ""
 	if version == "" {
-		cmd := exec.Command("laravel", "new", project)
+		cmd = exec.Command("laravel", "new", project)
 	} else {
-		cmd := exec.Command("composer", "create-project", "--prefer-dist", "laravel/laravel:" + version,  project)
+		cmd = exec.Command("composer", "create-project", "--prefer-dist", "laravel/laravel:" + version,  project)
 	}
 	err := cmd.Run()
 	if err != nil {
